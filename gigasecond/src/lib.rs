922aad90f4ee219a94adf2109696a57f2347f5c0
Vytas <contact@vytas.dev>
2020-02-24 17:19:20 +0200

    gigasecond: use constant and Duration instead of Timestamp

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{DateTime, Duration, Utc};
+
+const GIGASECOND: i64 = 1_000_000_000;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    Utc.timestamp(start.timestamp() + 1_000_000_000, 0)
+    start + Duration::seconds(GIGASECOND)
 }
